035a673daad98e5a2c20fc14c011b2ead11102e8
mkosecki1 <74equinoX&>
2020-06-01 10:58:12 +0200

    Add register screen layout

---
+++ app/src/main/java/com/footballapp/ui/register/RegisterFragment.kt
@@ -0,0 +1,6 @@
+package com.footballapp.ui.register
+
+import androidx.fragment.app.Fragment
+
+class RegisterFragment : Fragment() {
+}
---
+++ app/src/main/java/com/footballapp/ui/register/RegisterViewModel.kt
@@ -0,0 +1,6 @@
+package com.footballapp.ui.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel: ViewModel() {
+}
